f0dba7c7e366546d13b2183bc553b2e65b58a4bc
Charan Teja <68527259+itscharanteja@users.noreply.github.com>
2023-11-16 20:18:11 +0530

    first commit angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [
+const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
 ];
